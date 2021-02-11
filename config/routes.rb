5caffaf5209e7b7bb7a72b14a079e72e21769b65
miu oohira <miu.000408@gmail.com>
2021-02-12 00:21:01 +0900

    詳細画面から編集・削除機能の追加

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   get 'posts/:id' => "posts#show"
   get 'posts/:id/edit' => "posts#edit"
   post 'posts/create' =>"posts#create"
+  post 'posts/:id/update' =>"posts#update"
+  post 'posts/:id/destroy' =>"posts#destroy"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
